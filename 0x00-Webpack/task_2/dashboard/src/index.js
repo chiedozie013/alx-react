b675276c16f0412c3a4c33e34f48d4d83f04f147
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-07-29 04:35:51 +0100

    update on task 2

--- 0x00-Webpack/task_2/dashboard/src/index.js
+++ 0x00-Webpack/task_2/dashboard/src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Notifications from "./Notifications";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Notifications />
     <App />
   </React.StrictMode>
 );
