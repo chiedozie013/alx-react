86c4963800523cdbf052b5e785a87b696ec6041d
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-07-29 05:12:48 +0100

    task 4

--- 0x00-Webpack/task_4/dashboard/src/App.js
+++ 0x00-Webpack/task_4/dashboard/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email"></input>
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password"></input>
+          <button>OK</button>
+        </form>
+      </div>
+
+      <div className="App-footer">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {" "}
+          {getFullYear()} - {getFooterCopy()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
